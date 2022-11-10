2abfe91a64f37ab9c0a8633357620927604e7043
kristaponis <a.kristaponis@icloud.com>
2022-11-10 05:54:17 +0200

    Comments updates

--- contexts/usercontext.go
+++ contexts/usercontext.go
@@ -20,7 +20,7 @@ func WithUser(ctx context.Context, user *views.ViewUser) context.Context {
 
 // GetUser checks passed context value, makes type assertion to check
 // if the context value is of certain type, and the returns views.ViewUser.
-// This sets type of ViewUser, with less models.User fields.
+// This sets type of ViewUser, with fewer models.User fields.
 func GetUser(ctx context.Context) *views.ViewUser {
 	if value := ctx.Value(userKey); value != nil {
 		if user, ok := value.(*views.ViewUser); ok {
